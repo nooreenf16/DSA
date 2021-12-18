6fcf573b0ba052fbbb9de210e1868ce1dfcb78e4
nooreenf16 <83175847+nooreenf16@users.noreply.github.com>
2021-12-18 21:29:30 +0530

    Update TreeNode.cpp

--- Trees/TreeNode.cpp
+++ Trees/TreeNode.cpp
@@ -8,8 +8,18 @@ class TreeNode{
   TreeNode(T data){
     this->data = data;
   }
-  ~TreeNode(){
+  // delete tree using destructor
+  // in main-> delete root;
+  ~TreeNode(){ 
     for(int i=0; i<children.size(); i++)
       delete children[i];
   }
+}
+
+
+// delete tree using function
+void deleteTree(TreeNode <int>* root){
+  for(int i=0; i < root->children.size(); i++)
+    deleteTree(root->children[i]);
+  delete root;
 }
