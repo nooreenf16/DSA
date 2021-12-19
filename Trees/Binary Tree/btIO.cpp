ce5fec4b85bb54b24355618c8b79661bda8bbfee
nooreenf16 <83175847+nooreenf16@users.noreply.github.com>
2021-12-19 14:56:37 +0530

    Update btIO.cpp

--- Trees/Binary Tree/btIO.cpp
+++ Trees/Binary Tree/btIO.cpp
@@ -28,3 +28,61 @@ void printTree(btNode <int>* root){
   printTree(root->left);
   printTree(root->right);
 }
+
+// takeInput level wise
+btNode <int>* takeInputLevel(){
+  int rootData;
+  cout<<"Enter node data: ";
+  cin>>rootData;
+  if(rootData == -1)
+    return;
+  btNode <int>* root = new btNode(rootData);
+  queue<btNode<int>*> pendingNodes;
+  pendingNodes.push(root);
+  while(pendingNodes.size() != 0){
+    btNode <int>* front = pendingNodes.front();
+    pendingNodes.pop();
+    cout<<"Enter left child of "<<front->data;
+    int lcData;
+    cin>>lcData;
+    if(lcData != -1){
+      btNode <int>* leftChild = takeInputLevel();
+      pendingNodes.push(leftChild);
+      front->left = leftChild;
+    }
+    cout<<"Enter right child of"<<front->data;
+    int rcData;
+    cin>>rcData;
+    if(rcData != -1){
+      btNode <int>* rightChild = takeInputLevel();
+      pendingNodes.push(rightChild);
+      front->right = rightChild;
+    }
+  }
+  return root;
+}
+
+// printInput level wise
+void printInputLevel(btNode <int>* root){
+  if(root == NULL)
+    return;
+  queue<BinaryTreeNode<int>*> pending;
+  pending.push(root);
+  while(pending.size() != 0){
+      btNode<int>* front = pending.front();
+      pending.pop();
+      cout<<front->data<<":";
+      if(front->left == NULL)
+          cout<<"L:-1,";
+      else{
+          cout<<"L:"<<front->left->data<<",";
+          pending.push(front->left);
+      }
+      if(front->right == NULL)
+          cout<<"R:-1"<<endl;
+      else{
+          cout<<"R:"<<front->right->data<<endl;
+          pending.push(front->right);
+      }
+  }
+}
