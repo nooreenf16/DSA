ead007bd1810439683bd29016282edb9d14e121b
nooreenf16 <83175847+nooreenf16@users.noreply.github.com>
2021-12-20 23:22:06 +0530

    Update diameter.cpp

--- Trees/Binary Tree/diameter.cpp
+++ Trees/Binary Tree/diameter.cpp
@@ -1,4 +1,4 @@
-// method 1
+// method 1 : O(n*h)
 int height(btNode <int>* root){
   if(root == NULL)
     return 0;
@@ -13,4 +13,31 @@ int diameter(btNode<int>* root){
   int option3 = diameter(root->right);
   
   return max(option1, max(option2, option3));
+}
+
+// method 2 : O(n)
+// returning height and diameter simultaneously
+pair<int,int> heightDiameter(btNode <int>* root){
+  if(root == NULL){
+    pair<int,int> p;
+    p.first = 0;
+    p.second = 0;
+    return p;
+  }
+  pair<int,int> leftAns = heightDiameter(root->left);
+  pair<int,int> rightAns = heightDiameter(root->right);
+  
+  int ld = leftAns.second;
+  int rd = rightAns.second;
+  int lh = leftAns.first;
+  int rh = rightAns.first;
+  
+  int height = 1 + max(lh, rh);
+  int diameter = max(lh+rh, max(ld,rd));
+  
+  pair<int,int>p;
+  p.first = height;
+  p.second = diameter;
+  
+  return p;
 }
