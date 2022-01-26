964c9dce8b23d918f41514492b08b02bd0d9f2e6
nooreenf16 <83175847+nooreenf16@users.noreply.github.com>
2022-01-26 20:35:11 +0530

    Create minCount.cpp

---
+++ Dynamic Programming/minCount.cpp
@@ -0,0 +1,14 @@
+// brute force
+int minCount(int n){
+  if(n<=1)
+    return 0;
+  
+  int x = minCount(n-1);
+  
+  if(n%2 == 0)
+    int y = minCount(n%2);
+  if(n%3 == 0)
+    int z = minCount(n%3);
+  
+  return 1 + min(x, min(y,z));
+}
