139658d7b0365330eaa2de5589ad2446c73ad95d
nooreenf16 <83175847+nooreenf16@users.noreply.github.com>
2022-01-28 18:53:18 +0530

    Update minCount.cpp

--- Dynamic Programming/minCount.cpp
+++ Dynamic Programming/minCount.cpp
@@ -1,3 +1,5 @@
+// minimum number of steps to reach 1 from n by -1 or /2 or /3
+
 // brute force
 int minCount(int n){
   if(n<=1)
@@ -6,9 +8,9 @@ int minCount(int n){
   int x = minCount(n-1);
   
   if(n%2 == 0)
-    int y = minCount(n%2);
+    int y = minCount(n/2);
   if(n%3 == 0)
-    int z = minCount(n%3);
+    int z = minCount(n/3);
   
   return 1 + min(x, min(y,z));
 }
